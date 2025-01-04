ff7e9dba733a5901b254e7a3bdb4990f2542984a
Dantsuh <hirvonen.daniel@gmail.com>
2025-01-04 15:09:19 +0200

    Comments added to the main.py

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ def book():
         file_contents = f.read()
     return file_contents.lower()
 
-def split_to_words():
+def split_to_words(): # Function to split the book text into single words
     words = book().split()
     return words
 
-def word_count():
+def word_count(): # Function to count all the words in the book
     return len(split_to_words())
 
-def characters():
+def characters(): # Function to get all the single characters
     characters = []
     words = book().split()
     for word in words:
@@ -21,13 +21,13 @@ def characters():
     return characters
 
 def character_count(): # Count the amount of characters function
-    character_list = characters() # Every character from the book passed from the function characters()
+    character_list = characters()
     character_letter_list = []
-    for chara in character_list:
+    for chara in character_list: # Get only the characters from the alphabet
         if chara.isalpha():
             character_letter_list.append(chara)
     character_dictionary = {char: 0 for char in character_letter_list}
-    for char in character_letter_list:
+    for char in character_letter_list: # Get the count of each character to the dictionary
         if char in character_dictionary:
             character_dictionary[char] += 1
         else:
@@ -35,7 +35,7 @@ def character_count(): # Count the amount of characters function
     
     return character_dictionary
 
-def character_report():
+def character_report(): # Give the report for each character
     characters = character_count()
     for character in characters:
         count = characters[character]
